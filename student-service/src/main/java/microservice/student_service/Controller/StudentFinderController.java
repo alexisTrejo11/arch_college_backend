8f557e0f14b6d5dc35970ffccceffdd6482ba205
Alexis Trejo <marcoalexispt.02@gmail.com>
2025-01-30 19:07:37 -0600

    Improve user documentation

--- student-service/src/main/java/microservice/student_service/Controller/StudentFinderController.java
+++ student-service/src/main/java/microservice/student_service/Controller/StudentFinderController.java
@@ -2,13 +2,15 @@ package microservice.student_service.Controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import microservice.common_classes.DTOs.Student.StudentDTO;
 import microservice.common_classes.Utils.Response.ResponseWrapper;
 import microservice.common_classes.Utils.Response.Result;
 import microservice.common_classes.Utils.Student.StudentFilter;
+import microservice.student_service.Documentation.ApiResponseExamples;
 import microservice.student_service.Service.StudentFinderService;
 import microservice.student_service.Utils.StudentFinderValidator;
 import org.springframework.data.domain.Page;
@@ -28,13 +30,18 @@ public class StudentFinderController {
 
     private final StudentFinderService studentFinderService;
     private final StudentFinderValidator studentFinderValidator;
-    
+
     @GetMapping("/{studentId}")
-    @Operation(summary = "Get student by ID", description = "Fetches a student by their ID.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Student data successfully fetched"),
-            @ApiResponse(responseCode = "404", description = "Student not found")
-    })
+    @Operation(
+            summary = "Get student by ID",
+            description = "Fetches a student by their ID.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Student data successfully fetched",
+                            content = @Content(mediaType = "application/json", examples = @ExampleObject(value = ApiResponseExamples.STUDENT_FETCHED))),
+                    @ApiResponse(responseCode = "404", description = "Student not found",
+                            content = @Content(mediaType = "application/json", examples = @ExampleObject(value = ApiResponseExamples.STUDENT_NOT_FOUND)))
+            }
+    )
     public ResponseEntity<ResponseWrapper<StudentDTO>> getStudentById(@PathVariable Long studentId) {
         Result<StudentDTO> studentResult = studentFinderService.getStudentById(studentId);
         if (!studentResult.isSuccess()) {
@@ -44,9 +51,18 @@ public class StudentFinderController {
         return ResponseEntity.ok(ResponseWrapper.ok(studentResult.getData(), "Student data successfully fetched"));
     }
 
-    @Operation(summary = "Get student by account number", description = "Fetches a student by their account number.")
+    @Operation(
+            summary = "Get student by account number",
+            description = "Fetches a student by their account number.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Student data successfully fetched",
+                            content = @Content(mediaType = "application/json", examples = @ExampleObject(value = ApiResponseExamples.STUDENT_FETCHED))),
+                    @ApiResponse(responseCode = "404", description = "Student not found",
+                            content = @Content(mediaType = "application/json", examples = @ExampleObject(value = ApiResponseExamples.STUDENT_NOT_FOUND)))
+            }
+    )
     @GetMapping("/by-accountNumber/{accountNumber}")
-    public ResponseEntity<ResponseWrapper<StudentDTO>> getStudentById(@PathVariable String accountNumber) {
+    public ResponseEntity<ResponseWrapper<StudentDTO>> getStudentByAccountNumber(@PathVariable String accountNumber) {
         Result<StudentDTO> studentResult = studentFinderService.getStudentByAccountNumber(accountNumber);
         if (!studentResult.isSuccess()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ResponseWrapper.notFound(studentResult.getErrorMessage()));
@@ -55,16 +71,6 @@ public class StudentFinderController {
         return ResponseEntity.ok(ResponseWrapper.ok(studentResult.getData(), "Student data successfully fetched"));
     }
 
-
-    /**
-     * Endpoint to get all students with pagination and sorting based on a specific filter.
-     *
-     * @param page The page number for pagination.
-     * @param size The number of elements per page.
-     * @param filter The filter by which the students will be sorted (e.g., "SEMESTERS_COMPLETED").
-     * @param sortOrder The sort order, can be "asc" for ascending or "desc" for descending.
-     * @return ResponseEntity containing the paginated and filtered list of students.
-     */
     @Operation(
             summary = "Get all students sorted by a specific filter",
             description = "This endpoint retrieves a paginated list of students sorted by a specific filter.",
@@ -73,6 +79,12 @@ public class StudentFinderController {
                     @Parameter(name = "size", description = "Number of items per page", example = "10", required = false),
                     @Parameter(name = "filter", description = "Filter by which the students will be sorted", example = "SEMESTERS_COMPLETED", required = false),
                     @Parameter(name = "sortOrder", description = "Sort order, can be 'asc' or 'desc'", example = "asc", required = false)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Student data successfully fetched with filter",
+                            content = @Content(mediaType = "application/json", examples = @ExampleObject(value = ApiResponseExamples.STUDENTS_FETCHED))),
+                    @ApiResponse(responseCode = "400", description = "Invalid filter",
+                            content = @Content(mediaType = "application/json", examples = @ExampleObject(value = ApiResponseExamples.INVALID_FILTER)))
             }
     )
     @GetMapping("/all")
@@ -92,22 +104,11 @@ public class StudentFinderController {
         Sort.Direction direction = Sort.Direction.fromString(sortOrder);
         Pageable pageable = PageRequest.of(page, size, Sort.by(direction, studentFilter.getEntityField()));
 
-        Page<StudentDTO> studentDTOS = studentFinderService.getAllStudentsSortedByFilterPageable(pageable,studentFilter);
+        Page<StudentDTO> studentDTOS = studentFinderService.getAllStudentsSortedByFilterPageable(pageable, studentFilter);
 
         return ResponseEntity.ok(ResponseWrapper.ok(studentDTOS, "Student data successfully fetched with filter: " + filter + " ("+  sortOrder +")"));
     }
 
-
-    /**
-     * Endpoint to get students filtered by a specific value with pagination.
-     *
-     * @param page The page number for pagination.
-     * @param size The number of elements per page.
-     * @param filter The filter to apply (e.g., "SEMESTERS_COMPLETED").
-     * @param param The value to use with the filter for the search.
-     * @param sortOrder The sort order, can be "asc" for ascending or "desc" for descending.
-     * @return ResponseEntity containing the paginated and filtered list of students.
-     */
     @Operation(
             summary = "Get students filtered by a specific value with pagination",
             description = "This endpoint retrieves a paginated list of students filtered by a specific value.",
@@ -117,6 +118,12 @@ public class StudentFinderController {
                     @Parameter(name = "filter", description = "Filter to apply", example = "SEMESTERS_COMPLETED", required = true),
                     @Parameter(name = "param", description = "Value to use with the filter for the search", example = "5", required = true),
                     @Parameter(name = "sortOrder", description = "Sort order, can be 'asc' or 'desc'", example = "asc", required = false)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Student data successfully fetched with filter",
+                            content = @Content(mediaType = "application/json", examples = @ExampleObject(value = ApiResponseExamples.STUDENTS_FETCHED))),
+                    @ApiResponse(responseCode = "404", description = "Invalid filter",
+                            content = @Content(mediaType = "application/json", examples = @ExampleObject(value = ApiResponseExamples.INVALID_FILTER)))
             }
     )
     @GetMapping("/by")
